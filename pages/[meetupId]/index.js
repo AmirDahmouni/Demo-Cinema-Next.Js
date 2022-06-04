6c4b20ec954c3141148947f3a1002b3ce44bf321
Amir <dahmouni.amir@gmail.com>
2022-06-04 10:53:29 +0100

    static paths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,15 +4,38 @@ import {useRouter} from "next/router"
 import { useState,useEffect } from "react";
 
 
+//we can't use react hhoks inside getStaticProps or getServerSideProps
+export async function getStaticProps(context){
+  const meetupId=context.params.meetupId;
+  console.log(meetupId)
+  return {
+    props :{
+
+    }
+  }
+}
+
+export async function getStaticPaths()
+{
+  return {
+    fallback:false,
+    paths: [{
+      params :{
+        meetupId:"m1"
+      }
+    }]
+  }
+}
 
 export default function MeetupDetails()
 {
-        const router=useRouter()
-        const [meetUp,setMeetUp]=useState(null)
-        useEffect(()=>{
+    const router=useRouter()
+    const [meetUp,setMeetUp]=useState(null)
+    useEffect(()=>{
           setMeetUp(DUMMY_MEETUPS.find(meet=>meet.id==router.query.meetupId))
-        },[])
+    },[])
         
-        return ( meetUp && <MeetupDetail Meetup={meetUp} />)
+    return ( meetUp && <MeetupDetail Meetup={meetUp} />)
         
 }
+
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
 import MeetupList from "../components/meetups/MeetupList"
 import DUMMY_MEETUPS from "./api/MeetUps"
 
+// executed after the build and deploiement
 export async function getStaticProps(){
   return {
     props: {
        meetups: DUMMY_MEETUPS
-    }
+    },
+    revalidate:10
   };
 }
+//for every incomming request to the server dynamically
+/*
+export async function getServerSideProps(context){
+  const req=context.req;
+  const res=context.res;
+  return {
+    props:{
+      meetups: DUMMY_MEETUPS
+    }
+  };
+}*/
 
 export default function HomePage(props) {
   return (
       <MeetupList meetups={props.meetups}/>   
   )
-
 }
 
 
