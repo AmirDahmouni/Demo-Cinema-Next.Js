c1bf26a8d46543ad82ecc395fa8a0210e5b682cc
Amir <dahmouni.amir@gmail.com>
2022-06-11 16:12:29 +0100

    improvement api

--- pages/api/new-meetup.js.js
+++ pages/api/new-meetup.js.js
@@ -3,21 +3,25 @@
 import {MongoClient} from "mongodb"
 
 export default async function handler(req, res) {
-  // if( req.method=='POST' )
-  const {title,image,address,}=req.body
-  const client=await MongoClient.connect("mongodb://localhost/movies")
-  const db=client.db()
-
-  const meetupCollections=db.collection("meetups")
-  try{
-    const result=await meetupCollections.insertOne(req.body)
-  }
-  catch(ex)
-  {
-    return res.status(400).json({ error:ex.message })
-  }
+  if( req.method=='POST' )
   
-  client.close()
+  {
+    const data=req.body
+    const {title,image,address,}=data
+    const client=await MongoClient.connect("mongodb://localhost/movies")
+    const db=client.db()
 
-  return res.status(200).json({ message:"success fetching movies",result })
+    const meetupCollections=db.collection("meetups")
+    try{
+     const result=await meetupCollections.insertOne(data)
+    }
+    catch(ex)
+    {
+      return res.status(400).json({ error:ex.message })
+    }
+  
+    client.close()
+  
+    return res.status(200).json({ message:"success fetching movies",result })
+  }
 }
